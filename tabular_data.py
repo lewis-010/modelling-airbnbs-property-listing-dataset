ae5d34ebd43bf3bc41726e6061c20d3d9f872be7
Lewis Cox <lcox101101@gmail.com>
2022-12-09 13:41:39 +0000

    create .py file for cleaning dataset

---
+++ tabular_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def remove_rows_with_missing_data():
+     df.dropna(subset=['Description', 'Cleanliness_rating', 'Accuracy_rating', 'Communication_rating', 'Location_rating', 'Check-in_rating', 'Value_rating'], inplace=True)
+
+def combine_description_strings():    
+    df['Description'] = df['Description'].str.replace('About this space', '')
+    df['Description'] = df['Description'].apply(lambda x: [item for item in eval(x) if item != ''])
+    df['Description'] = df['Description'].apply(lambda x: ''.join(x))
+
+def set_default_feature_values():
+    df.update(df[['guests', 'bedrooms', 'beds', 'bathrooms']].fillna(1))
+
+def clean_tabular_data():
+    remove_rows_with_missing_data()
+    combine_description_strings()
+    set_default_feature_values()
+
+if __name__=='__main__':
+    df = pd.read_csv('tabular_data/AirBnbData.csv')
+    df.drop(df.columns[19], axis=1, inplace=True) # removes unecessary final column
+    clean_tabular_data()
+    df.to_csv('clean_tabular_data.csv')
