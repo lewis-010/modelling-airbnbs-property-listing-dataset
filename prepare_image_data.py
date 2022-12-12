90f189140b4db7725425d6ab9a74c953b6776c33
Lewis Cox <lcox101101@gmail.com>
2022-12-12 13:48:54 +0000

    upload subdirectorires to s3 bucket

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -2,14 +2,25 @@ import boto3
 import os
 
 s3_client = boto3.client('s3')
+dir_path = r'C:\Users\lcox1\Documents\VSCode\AiCore\Data science\images' # path containing folders to upload
 
-bucket_name = 'aicore-airbnb-images'
-folder_path = 'C:\Users\lcox1\Documents\VSCode\AiCore\Data science\images'
+parent_bucket_name = 'aicore-airbnb-images' # parent bucket to upload the folders to
 
-for file_name in os.listdr(folder_path):
-    file_path = os.path.join(folder_path , file_name)
+# iterate through the subdirectories in the directory
+for root, dirs, files in os.walk(dir_path):
+    
+    # name of subdirectory
+    subdir_name = os.path.basename(root)
+    
+    # create new bucket for subdirectory
+    subdir_bucket_name = f'{parent_bucket_name}/{subdir_name}'
+    s3_client.create_bucket(Bucket = subdir_bucket_name)
 
-    key = file_name
+    # iterate through files in subdirectories
+    for file_name in files:
+        file_path = os.path.join(root, file_name)
 
-    s3_client.upload_file(file_path, bucket_name, key)
+        key = file_path
+
+        s3_client.upload_file(file_path, bucket_name, key)
 
