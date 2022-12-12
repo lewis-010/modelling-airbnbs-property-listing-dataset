59eddfbe7355d76b2cea505a9ff70a9aed217aea
Lewis Cox <lcox101101@gmail.com>
2022-12-12 15:59:54 +0000

    add access key and region to bucket

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -1,23 +1,18 @@
 import boto3
+import os
 
-# Set the AWS access key ID and secret access key for your boto3 client
-aws_access_key_id = "AKIAXKFMUG3C63ZJ344K"
-aws_secret_access_key = "azdTL4zRcDXPSJYRp8jNJ9d6rOuD6ENpBUa/xxLI"
+# Set up the S3 client
+s3 = boto3.client('s3', aws_access_key_id='AKIAXKFMUG3C63ZJ344K', aws_secret_access_key='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', region_name='eu-west-2')
 
-# Set the name of the bucket that you want to download from
-bucket_name = "aicore-airbnb-images"
+# List all the folders in the bucket
+response = s3.list_objects_v2(Bucket='my-bucket')
+folders = [item['Key'] for item in response['Contents'] if item['Key'].endswith('/')]
 
-# Create a boto3 client for the S3 service
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-)
+# Create the local directory
+local_dir = r'C:\Users\lcox1\Documents\VSCode\AiCore\Data science\images'
+if not os.path.exists(local_dir):
+    os.makedirs(local_dir)
 
-# List all objects in the bucket
-response = s3.list_objects_v2(Bucket=bucket_name)
-
-# Loop through the list of objects and download each one
-for obj in response["Contents"]:
-    key = obj["Key"]
-    s3.download_file(bucket_name, key, key)
+# Iterate through the folders and download them to the local directory
+for folder in folders:
+    s3.download_file('aicore-airbnb-images', folder, folder)
