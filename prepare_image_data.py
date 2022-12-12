73c545e02bdda32278f9f7d12d45665a309898cd
Lewis Cox <lcox101101@gmail.com>
2022-12-12 13:27:01 +0000

    upload image data to s3 bucket

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -1,2 +1,15 @@
-def download_images():
-    a
+import boto3
+import os
+
+s3_client = boto3.client('s3')
+
+bucket_name = 'aicore-airbnb-images'
+folder_path = 'C:\Users\lcox1\Documents\VSCode\AiCore\Data science\images'
+
+for file_name in os.listdr(folder_path):
+    file_path = os.path.join(folder_path , file_name)
+
+    key = file_name
+
+    s3_client.upload_file(file_path, bucket_name, key)
+
