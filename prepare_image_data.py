5a3d352942c013ac1e0c92a0b6da16f6f172be28
Lewis Cox <lcox101101@gmail.com>
2022-12-12 15:26:11 +0000

    download images from s3 bucket

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -1,4 +1,23 @@
 import boto3
 
-def download_images():
-    a
+# Set the AWS access key ID and secret access key for your boto3 client
+aws_access_key_id = "AKIAXKFMUG3C63ZJ344K"
+aws_secret_access_key = "azdTL4zRcDXPSJYRp8jNJ9d6rOuD6ENpBUa/xxLI"
+
+# Set the name of the bucket that you want to download from
+bucket_name = "aicore-airbnb-images"
+
+# Create a boto3 client for the S3 service
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+)
+
+# List all objects in the bucket
+response = s3.list_objects_v2(Bucket=bucket_name)
+
+# Loop through the list of objects and download each one
+for obj in response["Contents"]:
+    key = obj["Key"]
+    s3.download_file(bucket_name, key, key)
