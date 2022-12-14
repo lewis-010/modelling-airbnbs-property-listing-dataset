4af628ba71fdcc94239e3439cc6385f55565dcf4
Lewis Cox <lcox101101@gmail.com>
2022-12-14 13:57:03 +0000

    create function to resize images

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -1,20 +1,23 @@
 import boto3
-import os
 
+def download_images():
 # Set up the S3 client
-s3 = boto3.client('s3')
+    s3 = boto3.client('s3')
 
-# List all the folders in the bucket
-response = s3.list_objects_v2(Bucket='aicore-airbnb-images')
+    # List all the folders in the bucket
+    response = s3.list_objects_v2(Bucket='aicore-airbnb-project')
 
-folders = []
-for item in response['Contents']:
-    if item['Key'].endswith('/'):
-        folders.append(item['Key'])
+    folders = []
+    for item in response['Contents']:
+        if item['Key'].endswith('/'):
+            folders.append(item['Key'])
 
-# Create the local directory
-local_dir = r'C:\Users\lcox1\Documents\VSCode\AiCore\Data science\images'
+    # Create the local directory
+    local_dir = r'C:\Users\lcox1\Documents\VSCode\AiCore\Data science\images'
 
-# Iterate through the folders and download them to the local directory
-for folder in folders:
-    s3.download_file('aicore-airbnb-images', Key=folder, Filename=local_dir)
+    # Iterate through the folders and download them to the local directory
+    for folder in folders:
+        s3.download_file('aicore-airbnb-images', Key=folder, Filename=local_dir)
+
+def resize_images():
+    s
