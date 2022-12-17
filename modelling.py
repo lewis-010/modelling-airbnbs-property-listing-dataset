395f71cafa5e97fe1a1ce113753e03c017d848ce
Lewis Cox <lcox101101@gmail.com>
2022-12-17 19:45:59 +0000

    create modelling.py file

---
+++ modelling.py
@@ -0,0 +1,12 @@
+from tabular_data import *
+import sklearn
+
+# Create an instance of the SGDRegressor class
+sgd_regressor = SGDRegressor(loss='squared_loss')
+
+# Fit the model to the training data
+sgd_regressor.fit(features, labels)
+
+
+# Use the model to make predictions on new data
+predictions = sgd_regressor.predict(new_data)
