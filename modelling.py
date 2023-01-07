01d00542631f81d483355300db37fa5f6be2b1dc
Lewis Cox <lcox101101@gmail.com>
2023-01-07 14:18:37 +0000

    output the no. of samples in testing & training

--- modelling.py
+++ modelling.py
@@ -10,6 +10,12 @@ features, label = load_airbnb()
 # random_state ensures same random samples of data will be used for training & testing each time code is run
 X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.2, random_state=42)
 
+print(f'Number of samples in datase: {len(features)}')
+print('Number of samples in:')
+print(f'    training: {len(X_train)}')
+print(f'    testing: {len(X_test)}')
+
+
 model = SGDRegressor()
 model.fit(X_train, y_train)
 
