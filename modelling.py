9d3ac4504bc8d0f6c4f50c81d3f7abd7d183806f
Lewis Cox <lcox101101@gmail.com>
2023-01-07 14:40:38 +0000

    add validation set and remove random_state var

--- modelling.py
+++ modelling.py
@@ -7,14 +7,16 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 features, label = load_airbnb()
 
 # use 80% of data for training and 20% of the data for testing
-# random_state ensures same random samples of data will be used for training & testing each time code is run
-X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.2)
+
+# validation set used to compare models
+X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test, test_size=0.2)
 
 print(f'Number of samples in datase: {len(features)}')
 print('Number of samples in:')
-print(f'    training: {len(X_train)}')
-print(f'    testing: {len(X_test)}')
-
+print(f'    training: {len(y_train)}')
+print(f'    validation: {len(y_validation)}')
+print(f'    testing: {len(y_test)}')
 
 model = SGDRegressor()
 model.fit(X_train, y_train)
@@ -25,3 +27,5 @@ y_pred = model.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
+print(mse)
+print(mae)
