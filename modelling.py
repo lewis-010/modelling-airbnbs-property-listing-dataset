b27673fd0d5ddf2fca481a98a1c597c2bd3cb28a
Lewis Cox <lcox101101@gmail.com>
2023-01-09 16:30:07 +0000

    include r^2 evaluation

--- modelling.py
+++ modelling.py
@@ -2,19 +2,20 @@ import numpy as np
 from tabular_data import *
 from sklearn.linear_model import SGDRegressor
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-
 
 features, label = load_airbnb()
 X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3)
 X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test, test_size=0.5)
 
 print(f'Number of samples in dataset: {len(features)}')
-print('Number of samples in:')
-print(f'    training: {len(y_train)}')
-print(f'    validation: {len(y_validation)}')
-print(f'    testing: {len(y_test)}')
+print(
+    'Number of samples in: '
+    f'training: {len(y_train)}, '
+    f'validation: {len(y_validation)}, '
+    f'testing: {len(y_test)}, '
+)
 
 
 # normalize the features 
@@ -38,8 +39,8 @@ y_validation_pred = model.predict(X_validation_scaled) # validation set used to 
 y_test_pred = model.predict(X_test_scaled) # test set used to estimate how the model will perform on unseen (real world) data
 
 
-# evaluate model
-train_loss = mean_squared_error(y_train, y_train_pred)
+# evaluate model using RMSE
+train_loss = mean_squared_error(y_train, y_train_pred) # should I be evaluating using the training set??
 train_rmse = np.sqrt(train_loss)
 
 validation_loss = mean_squared_error(y_validation, y_validation_pred)
@@ -52,4 +53,15 @@ print(
     f'Train_rmse: {train_rmse}, ' 
     f'Validation_rmse: {validation_rmse}, '
     f'Test_rmse: {test_rmse}'
+)
+
+# evaluate model using R^2
+train_r2 = r2_score(y_train, y_train_pred) # should I be evaluating using the training set??
+validation_r2 = r2_score(y_validation, y_validation_pred)
+test_r2 = r2_score(y_test, y_test_pred)
+
+print(
+    f'Training_r2: {train_r2}, '
+    f'Validation_r2 {validation_r2}, '
+    f'Test_r2: {test_r2}'
 )
