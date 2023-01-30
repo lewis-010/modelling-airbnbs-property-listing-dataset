b4ac916d9a4d98925e37db072d149487661434be
Lewis Cox <lcox101101@gmail.com>
2023-01-30 14:48:06 +0000

    split dataset and create dataloaders for each set

--- nueralnet.py
+++ nueralnet.py
@@ -1,6 +1,7 @@
 import torch
 import pandas as pd
 from tabular_data import *
+from torch.utils.data import Dataset, DataLoader, random_split
 
 
 class AirbnbNightlyPriceImageDataset(Dataset):
@@ -13,8 +14,22 @@ class AirbnbNightlyPriceImageDataset(Dataset):
     
     def __getitem__(self, idx):
         features = self.features.iloc[idx]
-        features = torch.tensor(features)
-        label = self.label.iloc[idx]
+        features = torch.tensor(features, dtype=torch.float32)
+        label = self.label.iloc[idx].item()
         return (features, label)
 
+dataset = AirbnbNightlyPriceImageDataset()
 
+# split dataset into training, validation & test sets
+train_set, test_set = random_split(dataset, [int(len(dataset) * 0.7), len(dataset) - int(len(dataset) * 0.7)])
+train_set, validation_set = random_split(train_set, [int(len(train_set) * 0.5), len(train_set) - int(len(train_set) * 0.5)])
+print(
+    f'Number of samples in training set: {len(train_set)}, '
+    f'Number of samples in validation set: {len(validation_set)}, '
+    f'Number of samples in test set: {len(test_set)}'
+)
+
+# create dataloaders for each set
+train_dataloader = DataLoader(train_set, batch_size=32, shuffle=True)
+val_dataloader = DataLoader(validation_set, batch_size=32, shuffle=True)
+test_dataloader = DataLoader(test_set, batch_size=32, shuffle=True)
