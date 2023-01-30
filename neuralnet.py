0ab62e0c61c73b0211a79a5f85ca51945b237f8c
Lewis Cox <lcox101101@gmail.com>
2023-01-30 15:02:01 +0000

    change index locator to numpy

--- neuralnet.py
+++ neuralnet.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset, DataLoader, random_split
 
 
 class AirbnbNightlyPriceImageDataset(Dataset):
-    def __init__(self):
+    def __init__(self) -> None:
         self.data = pd.read_csv('tabular_data/clean_tabular_data.csv')
         self.features, self.label = load_airbnb(self.data, 'Price_Night')
 
@@ -13,9 +13,9 @@ class AirbnbNightlyPriceImageDataset(Dataset):
         return len(self.data)
     
     def __getitem__(self, idx):
-        features = self.features.iloc[idx]
+        features = self.features[idx,:]
         features = torch.tensor(features, dtype=torch.float32)
-        label = self.label.iloc[idx].item()
+        label = self.label[idx].item()
         return (features, label)
 
 dataset = AirbnbNightlyPriceImageDataset()
@@ -30,6 +30,14 @@ print(
 )
 
 # create dataloaders for each set
-train_dataloader = DataLoader(train_set, batch_size=32, shuffle=True)
-val_dataloader = DataLoader(validation_set, batch_size=32, shuffle=True)
-test_dataloader = DataLoader(test_set, batch_size=32, shuffle=True)
+train_loader = DataLoader(train_set, batch_size=8, shuffle=True)
+val_loader = DataLoader(validation_set, batch_size=8, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=8, shuffle=True)
+
+example = next(iter(train_loader))
+print(example)
+
+
+class NeuralNet:
+    def __init__(self) -> None:
+        pass
