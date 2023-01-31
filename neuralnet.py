e8ff737c443c354dcf50c338030f376ff1b90f56
Lewis Cox <lcox101101@gmail.com>
2023-01-31 18:45:01 +0000

    first successfull neural net model

--- neuralnet.py
+++ neuralnet.py
@@ -7,20 +7,21 @@ from torch.utils.data import Dataset, DataLoader, random_split
 
 
 class AirbnbNightlyPriceImageDataset(Dataset):
-    def __init__(self) -> None:
+    def __init__(self):
         self.data = pd.read_csv('tabular_data/clean_tabular_data.csv')
         self.features, self.label = load_airbnb(self.data, 'Price_Night')
-
-    def __len__(self):
-        return len(self.data)
     
     def __getitem__(self, idx):
-        features = self.features[idx,:]
-        features = torch.tensor(features, dtype=torch.float32)
-        label = self.label[idx].item()
-        return (features, label)
+        return (torch.tensor(self.features[idx]), self.label[idx])
+
+    def __len__(self):
+        return len(self.features)
  
 dataset = AirbnbNightlyPriceImageDataset()
+
+print(dataset[10])
+print(len(dataset))
+print(dataset.features.dtype)
 
 # split dataset into training, validation & test sets
 train_set, test_set = random_split(dataset, [int(len(dataset) * 0.7), len(dataset) - int(len(dataset) * 0.7)])
@@ -32,13 +33,10 @@ print(
 )
 
 # create dataloaders for each set
-train_loader = DataLoader(train_set, batch_size=8, shuffle=True)
-val_loader = DataLoader(validation_set, batch_size=8, shuffle=True)
-test_loader = DataLoader(test_set, batch_size=8, shuffle=True)
-
-dataset.features, dataset.label = next(iter(train_loader))
-print(dataset.features, dataset.label)
-
+batch_size = 8
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
 # define neural network architecture
 class NN(nn.Module):
@@ -55,3 +53,9 @@ class NN(nn.Module):
     def forward(self, X):
         # returns prediction by passing X through all layers
         return self.layers(X)
+
+model = NN()
+input_tensor = torch.tensor(dataset.features, dtype=torch.float32)
+model(input_tensor)
+
+
