6c0c44ec67b033dc2e9e7574905b0a4d342dc13a
Lewis Cox <lcox101101@gmail.com>
2023-02-06 11:19:36 +0000

    add optimiser step  to training loop

--- neuralnet.py
+++ neuralnet.py
@@ -16,20 +16,16 @@ class AirbnbNightlyPriceImageDataset(Dataset):
 
     def __len__(self):
         return len(self.features)
- 
-dataset = AirbnbNightlyPriceImageDataset()
 
-print(dataset[10])
-print(len(dataset))
-print(dataset.features.dtype)
+dataset = AirbnbNightlyPriceImageDataset()
 
 # split dataset into training, validation & test sets
 train_set, test_set = random_split(dataset, [int(len(dataset) * 0.7), len(dataset) - int(len(dataset) * 0.7)])
 test_set, validation_set = random_split(test_set, [int(len(test_set) * 0.5), len(test_set) - int(len(test_set) * 0.5)])
 print(
-    f'Number of samples in training set: {len(train_set)}, '
-    f'Number of samples in validation set: {len(validation_set)}, '
-    f'Number of samples in test set: {len(test_set)}'
+    f'Training set: {len(train_set)} samples, '
+    f'Validation set: {len(validation_set)} samples, '
+    f'Test set: {len(test_set)} samples'
 )
 
 # create dataloaders for each set
@@ -59,16 +55,20 @@ model = NN()
 # model(input_tensor)
 
 def train(model, epochs=10):
+
+    optimiser = torch.optim.Adam(model.parameters(), lr= 0.001)
+
     for epoch in range(epochs):
         for batch in train_loader:
             features, labels = batch
             features = features.type(torch.float32)
-            print(features.dtype)
-            print(labels.dtype)
             labels = torch.unsqueeze(labels, 1)
             prediction = model(features)
             loss = F.mse_loss(prediction, labels.float())
             loss.backward()
-            print(loss)
+            print(loss.item())
+            # optimisation step
+            optimiser.step() 
+            optimiser.zero_grad()
 
 train(model)
